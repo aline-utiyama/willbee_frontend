e5a31097f09a6f89838bd061d9b81800e3453c93
aline utiyama <lineskutiyama@gmail.com>
2025-01-23 17:03:09 +0900

    Add Dashboard page container

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -13,7 +13,18 @@ const Dashboard = () => {
 
   return (
     <div>
-      Welcome to your dashboard! <button onClick={handleLogout}>Logout</button>;
+      <header className="bg-white shadow-sm">
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+            Dashboard
+          </h1>
+        </div>
+      </header>
+      <main>
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          Welcome to your dashboard!
+        </div>
+      </main>
     </div>
   );
 };
