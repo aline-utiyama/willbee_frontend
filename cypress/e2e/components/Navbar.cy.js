cc0482f3a94efebcca177fd55542a9e7f077694c
Danica Rachel Lim <drachel.lim@gmail.com>
2025-01-31 14:54:20 +0800

    initial commit

---
+++ cypress/e2e/components/Navbar.cy.js
@@ -0,0 +1,42 @@
+describe('Navbar', () => {
+  it('should display login and signup buttons on the homepage', () => {
+    cy.visit('http://localhost:3000/');
+
+    cy.get('button').contains('Login').should('be.visible');
+    cy.get('button').contains('Signup').should('be.visible');
+  });
+
+  it('should navigate to the login page when the login button is clicked', () => {
+    cy.visit('http://localhost:3000/');
+
+    cy.get('button').contains('Login').click();
+
+    cy.url().should('include', '/login');
+    cy.get('input[name="Email address"]').should('be.visible');
+    cy.get('input[name="Password"]').should('be.visible');
+  });
+
+  it('should display profile and logout buttons when logged in', () => {
+    // Simulate login
+    cy.visit('http://localhost:3000/dashboard');
+
+    cy.get('button').contains('View notifications').should('be.visible');
+    cy.get('button').contains('Open user menu').should('be.visible').click();
+
+    cy.get('a').contains('Your Profile').should('be.visible');
+    cy.get('a').contains('Settings').should('be.visible');
+    cy.get('button').contains('Sign out').should('be.visible');
+  });
+
+  it('should navigate to the login page when logout is clicked', () => {
+    // Simulate login
+    cy.visit('http://localhost:3000/dashboard');
+
+    cy.get('button').contains('Open user menu').click();
+    cy.get('button').contains('Sign out').click();
+
+    cy.url().should('include', '/login');
+  });
+
+
+});
