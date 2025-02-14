61bf3db9924ff7c68113da020c0e55c4e24a8b78
aline-utiyama <lineskutiyama@gmail.com>
2025-02-14 14:07:18 +0900

    Create Goal Plan Page

--- src/app/goal-plans/list/page.js
+++ src/app/goal-plans/list/page.js
@@ -1,7 +1,40 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import railsAPI from "@/services/rails-api";
+
 const GoalPlansList = () => {
+  const [goalPlans, setGoalPlans] = useState([]);
+  const [errors, setErrors] = useState("");
+  const router = useRouter();
+
+  const fetchGoals = async () => {
+    try {
+      const response = await railsAPI.get(`/goal_plans`);
+      setGoalPlans(response.data);
+    } catch (err) {
+      setErrors("Failed to fetch goals data");
+    }
+  };
+  useEffect(() => {
+    fetchGoals();
+  }, []);
   return (
-    <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+    <div className="mt-3 md:mx-auto md:w-full md:max-w-lg">
       Goal Plans List
+      {errors && <p style={{ color: "red" }}>{errors}</p>}
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-1 lg:grid-cols-1 mx-auto flex max-w-2xl items-center py-4 px-2">
+        {goalPlans &&
+          goalPlans.map((goalPlan) => (
+            <div
+              key={goalPlan.id}
+              onClick={() => router.push(`/goal-plans/${goalPlan.id}`)}
+              className="cursor-pointer"
+            >
+              {goalPlan.title}
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
