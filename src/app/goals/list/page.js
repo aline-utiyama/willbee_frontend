97f4c4c4098837a57f0d365122d409169f40491d
aline-utiyama <lineskutiyama@gmail.com>
2025-02-25 23:48:19 +0900

    Adjust container style, added a spinner and fixed the test

--- src/app/goals/list/page.js
+++ src/app/goals/list/page.js
@@ -3,10 +3,12 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import GoalCard from "../../components/GoalCard";
 import railsAPI from "@/services/rails-api";
+import Spinner from "@/app/components/Spinner";
 
 const GoalsList = () => {
   const [goals, setGoals] = useState([]);
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const router = useRouter();
 
@@ -14,13 +16,19 @@ const GoalsList = () => {
     try {
       const response = await railsAPI.get(`/goals`);
       setGoals(response.data);
+      setLoading(false);
     } catch (err) {
       setError("Failed to fetch goals data");
+      setLoading(false);
     }
   };
   useEffect(() => {
     fetchGoals();
   }, []);
+
+  if (loading) {
+    return <Spinner />;
+  }
 
   if (error) {
     return (
@@ -37,70 +45,71 @@ const GoalsList = () => {
   }
 
   return (
-    <div className="mt-3 md:mx-auto md:w-full md:max-w-lg">
-      <h1 className="text-2xl font-bold text-gray-900 mb-4">My Goals</h1>
-
-      <nav aria-label="Breadcrumb">
-        <ol
-          role="list"
-          className="mx-auto flex max-w-2xl items-center space-x-2 px-2"
-        >
-          <li>
-            <div className="flex items-center">
-              <a
-                href="/dashboard"
-                className="mr-2 text-md font-medium text-gray-900"
-              >
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="md:mx-auto md:w-full md:max-w-lg">
+        <nav aria-label="Breadcrumb">
+          <ol
+            role="list"
+            className="mx-auto flex max-w-2xl items-center space-x-2 px-2"
+          >
+            <li>
+              <div className="flex items-center">
+                <a
+                  href="/dashboard"
+                  className="mr-2 text-md font-medium text-gray-900"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="h-5 w-4 text-gray-600"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M9.293 2.293a1 1 0 0 1 1.414 0l7 7A1 1 0 0 1 17 11h-1v6a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-6H3a1 1 0 0 1-.707-1.707l7-7Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </a>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 20 20"
                   fill="currentColor"
-                  className="h-5 w-4 text-gray-600"
+                  className="h-6 w-4 text-gray-300"
                 >
                   <path
                     fillRule="evenodd"
-                    d="M9.293 2.293a1 1 0 0 1 1.414 0l7 7A1 1 0 0 1 17 11h-1v6a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-6H3a1 1 0 0 1-.707-1.707l7-7Z"
+                    d="M12.528 3.047a.75.75 0 0 1 .449.961L8.433 16.504a.75.75 0 1 1-1.41-.512l4.544-12.496a.75.75 0 0 1 .961-.449Z"
                     clipRule="evenodd"
                   />
                 </svg>
-              </a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="h-6 w-4 text-gray-300"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M12.528 3.047a.75.75 0 0 1 .449.961L8.433 16.504a.75.75 0 1 1-1.41-.512l4.544-12.496a.75.75 0 0 1 .961-.449Z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </div>
-          </li>
-          <li>
-            <div className="flex items-center">
-              <a
-                href="#"
-                aria-current="page"
-                className="font-medium text-gray-900 hover:text-gray-600"
-              >
-                My goals
-              </a>
-            </div>
-          </li>
-        </ol>
-      </nav>
-
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      <div className="space-y-4 justify-self-center">
-        {goals.map((goal) => (
-          <GoalCard
-            key={goal.id}
-            goal={goal}
-            onClick={() => router.push(`/goals/${goal.id}`)}
-          />
-        ))}
+              </div>
+            </li>
+            <li>
+              <div className="flex items-center">
+                <a
+                  href="#"
+                  aria-current="page"
+                  className="font-medium text-gray-900 hover:text-gray-600"
+                >
+                  My goals
+                </a>
+              </div>
+            </li>
+          </ol>
+        </nav>
+      </div>
+      <div className="mt-3 md:mx-auto md:w-full md:max-w-lg">
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        <div className="space-y-4 justify-self-center">
+          {goals.map((goal) => (
+            <GoalCard
+              key={goal.id}
+              goal={goal}
+              onClick={() => router.push(`/goals/${goal.id}`)}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
